0152fb709172a15bfd1ac6250fac13db750ff5c1
ILYAS <41228178+ILAI-BRO@users.noreply.github.com>
2022-11-29 22:52:23 +0300

    Create task__5.js

---
+++ task__5.js
@@ -0,0 +1,17 @@
+const arr = [true, 'string', 123, false, null]
+
+console.log(arr.length);
+
+remove = (arr.pop(), arr.shift());
+
+console.log(arr);
+
+add = (arr.unshift('Hello'), arr.push(17));
+
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
