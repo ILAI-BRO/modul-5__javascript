60da1abe611928ff1bee61b69b07fcff80a0c248
ILYAS <41228178+ILAI-BRO@users.noreply.github.com>
2022-11-29 22:52:02 +0300

    Update task__4.js

--- task__4.js
+++ task__4.js
@@ -1,20 +1,3 @@
 console.log(Math.floor(Math.random() * 101));
 
-Задание №5
-const arr = [true, 'string', 123, false, null]
 
-console.log(arr.length);
-
-remove = (arr.pop(), arr.shift());
-
-console.log(arr);
-
-add = (arr.unshift('Hello'), arr.push(17));
-
-console.log(arr);
-
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
-console.log(arr[4]);
